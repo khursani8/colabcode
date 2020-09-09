bc062d34b9ca37bb5c20993023bcb65646fd5661
khursani8 <khursani8@gmail.com>
2020-09-10 03:59:29 +0900

    verbose

--- colabcode/code.py
+++ colabcode/code.py
@@ -47,12 +47,13 @@ class ColabCode:
         if self._mount and colab_env:
             drive.mount("/content/drive")
             if self._drive_name:
-                folder = subprocess.run(["mkdir","-p", f"/content/{self._drive_name}"], stdout=subprocess.PIPE)
+                folder_path = f"/content/{self._drive_name}"
+                folder = subprocess.run(["mkdir","-p", folder_path], stdout=subprocess.PIPE)
                 if folder.stdout:
-                    print(folder.stdout)
-                mount = subprocess.run(["mount","--bind","/content/drive/My\ Drive",f"/content/{self._drive_name}"], stdout=subprocess.PIPE)
+                    print("folder:",folder.stdout)
+                mount = subprocess.run(["mount","--bind","/content/drive/My\ Drive",folder_path], stdout=subprocess.PIPE)
                 if mount:
-                    print(mount.stdout)
+                    print("mount",mount.stdout)
         if self.password:
             code_cmd = f"PASSWORD={self.password} code-server --port {self.port} --disable-telemetry"
         else:
